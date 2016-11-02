585c554a4938e99df538893c95d6336e5348b63e
Jonathan <mail@strfry.org>
2016-11-02 19:51:06 +0100

    cleanup

--- ewindow-menu.sh
+++ ewindow-menu.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+cd $(dirname $0) # Change working directory
+
 #svscan $(dirname $0) &
 
 #./send-localhost.sh >/dev/null &
 
 dialog --menu menutext 20 100 10 \
 	Preview "Show Local Camera Stream" \
-	tag2 item2 tag3 item3 \
+	Call "Holzwerkstatt" \
 	Exit "" \
 	2> dialog.result
 
@@ -16,9 +18,14 @@ fi
 
 case $(cat dialog.result) in
 	Preview*)
+		echo localhost > send/host
+		svc -d -u send
 		svc -u recv
 		dialog --msgbox "Started Camera Preview..." 20 100
 		svc -d recv
+		;;
+	Call*)
+		exit
 		;;
 	Exit*)
 		exit
